642951a83c93327612e5b4653957accd1e61c892
MFHamilton <mariahamiltonr1@gmail.com>
2024-11-16 19:54:06 -0500

    Program exercise #14

--- commandline.c
+++ commandline.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char *argv[]){
     int i;
-    printf("There were %d command line arguments provided:\n", arg_count);
-    for(i = 0; i < arg_count; i++){
-        printf("argument #%d\t-\t%s\n", i, arg_list[i]);
+    printf("There were %d command line arguments provided:\n", argc);
+    for(i = 0; i < argc; i++){
+        printf("argument #%d\t-\t%s\n", i, argv[i]);
     }
 }
